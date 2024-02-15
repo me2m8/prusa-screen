063e1da1d77e29e7fafcbebd459682b81022a5ad
me2m8 <elmer.brannstrom@gmail.com>
2024-02-15 23:25:25 +0100

    Added three containers that will slide to cover the screen

--- src/main.rs
+++ src/main.rs
@@ -6,21 +6,15 @@ fn main() {
 
 #[component]
 fn App() -> impl IntoView {
-    let (big, set_big) = create_signal("");
-
     view! {
-        <Sliding_container color="#FFFFFF" id="left" big set_big />
-        <Sliding_container color="#ff9200" id="right" big set_big />
+        <Sliding_container_wrapper />
     }
 }
 
 #[component]
-fn Sliding_container(
-    color: &'static str,
-    id: &'static str,
-    big: leptos::ReadSignal<&'static str>,
-    set_big: leptos::WriteSignal<&'static str>,
-) -> impl IntoView {
+fn Sliding_container(color: &'static str, name: &'static str) -> impl IntoView {
+    let (big, change_size) = create_signal(false);
+
     let style = format!("background-color: {}", color);
 
     view! {
@@ -28,15 +22,25 @@ fn Sliding_container(
             style=style
 
             class="sliding_container"
-            class:container_small=move || id != big()
-            class:container_big=move || id == big()
+            class:container_small=move || !big()
+            class:container_big=move || big()
 
             on:click=move |_| {
-                set_big(if id == big() {""} else {id});
+                change_size(!big());
                 console_log("pressed");
             }
             >
-            <p class="dissappearing_font">"Press when bed is cleared"</p>
+            <p class="disappearing_font name-tag">{name}</p>
+            <p class="disappearing_font">"Focus"</p>
         </div>
+    }
+}
+
+#[component]
+fn Sliding_container_wrapper() -> impl IntoView {
+    view! {
+        <Sliding_container color="#ffffff" name="Ruccola"/>
+        <Sliding_container color="#0092ff" name="middle"/>
+        <Sliding_container color="#ff9200" name="Lotta"/>
     }
 }
