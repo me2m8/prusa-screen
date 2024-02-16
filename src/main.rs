c683304e34694139f4b30daed351a3a69508e5f0
Elmer Brännström <06elbr01@minervagymnasium.se@06elbr01@minervagymnasium.se---@minervagymnasium.se---Elmer-Brannstrom>
2024-02-16 20:16:33 +0100

    Made the slider wrapper work

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use leptos::{component, create_signal, leptos_dom::logging::console_log, view, IntoView};
+use leptos::{component, create_signal, view, IntoView};
 
 fn main() {
     leptos::mount_to_body(move || view! { <App /> });
@@ -12,9 +12,7 @@ fn App() -> impl IntoView {
 }
 
 #[component]
-fn Sliding_container(color: &'static str, name: &'static str) -> impl IntoView {
-    let (big, change_size) = create_signal(false);
-
+fn HSliding_container(color: &'static str, name: &'static str, big: leptos::ReadSignal<bool>) -> impl IntoView {
     let style = format!("background-color: {}", color);
 
     view! {
@@ -25,10 +23,6 @@ fn Sliding_container(color: &'static str, name: &'static str) -> impl IntoView {
             class:container_small=move || !big()
             class:container_big=move || big()
 
-            on:click=move |_| {
-                change_size(!big());
-                console_log("pressed");
-            }
             >
             <p class="disappearing_font name-tag">{name}</p>
             <p class="disappearing_font">"Focus"</p>
@@ -38,9 +32,23 @@ fn Sliding_container(color: &'static str, name: &'static str) -> impl IntoView {
 
 #[component]
 fn Sliding_container_wrapper() -> impl IntoView {
+    let (big1, change_size1) = create_signal(false);
+    let (big2, change_size2) = create_signal(false);
+    let (big3, change_size3) = create_signal(false);
+
     view! {
-        <Sliding_container color="#ffffff" name="Ruccola"/>
-        <Sliding_container color="#0092ff" name="middle"/>
-        <Sliding_container color="#ff9200" name="Lotta"/>
+        <HSliding_container color="#ffffff" name="Ruccola" big=big1 on:click=move|_|{
+            change_size1(!big1());
+            change_size2(false);
+        }/>
+        <HSliding_container color="#0092ff" name="Information" big=big2 on:click=move|_|{
+            change_size1(false);
+            change_size2(!big2());
+            change_size3(false);
+        }/>
+        <HSliding_container color="#ff9200" name="Lotta" big=big3 on:click=move|_|{
+            change_size2(false);
+            change_size3(!big3());
+        }/>
     }
 }
